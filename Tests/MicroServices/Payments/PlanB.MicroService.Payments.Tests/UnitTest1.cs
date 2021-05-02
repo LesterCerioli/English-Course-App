5e879754e6d76c994ba21da7395511f04128d2f7
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-05-02 18:36:16 -0300

    "feat(#85): implementing Unit tests"

---
+++ Tests/MicroServices/Payments/PlanB.MicroService.Payments.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PlanB.MicroService.Payments.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
