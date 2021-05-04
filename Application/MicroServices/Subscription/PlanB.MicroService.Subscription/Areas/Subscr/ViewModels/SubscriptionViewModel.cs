9c2b7727bb6691d70d0600c6fa3bea902fd517a6
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-05-04 11:55:12 -0300

    "feat(#89): implementing Subscription View Model"

---
+++ Application/MicroServices/Subscription/PlanB.MicroService.Subscription/Areas/Subscr/ViewModels/SubscriptionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanB.MicroService.Subscription.Areas.Subscr.ViewModels
+{
+    public class SubscriptionViewModel
+    {
+    }
+}
