21e30f018f424c0054e0b1cbc529145a7510cc81
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-05-12 15:40:13 -0300

    fet(#99): implementando Services do microservice Core

--- Application/Modules/PlanB.Module.Core/Services/IMediaService.cs
+++ Application/Modules/PlanB.Module.Core/Services/IMediaService.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PlanB.Module.Core.Services
 {
-    interface IMediaService
+    public interface IMediaService
     {
+        string GetMediaUrl(Media media);
+
+        string GetMediaUrl(string fileName);
+
+        string GetThumbnailUrl(Media media);
+
+        Task SaveMediaAsync(Stream mediaBinaryStream, string fileName, string mimeType = null);
+
+        Task DeleteMediaAsync(Media media);
+
+        Task DeleteMediaAsync(string fileName);
     }
 }
