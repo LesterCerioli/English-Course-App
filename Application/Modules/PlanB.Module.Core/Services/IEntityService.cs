3db178a0270795c45f2ed4ec14de0f04768d9639
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-05-12 14:55:37 -0300

    feat(#102): implementando a interface IEntityService

---
+++ Application/Modules/PlanB.Module.Core/Services/IEntityService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanB.Module.Core.Services
+{
+    public interface IEntityService
+    {
+        string ToSafeSlug(string slug, long entityId, string entityTypeId);
+        IEntityService Get(long entityId, string entityTypeId);
+        void Add(string name, string slug, long entityId, string entityTypeid);
+        void Update(string newname, string newSlug, long entityid, string entityTypeid);
+        Task Remove(long entityId, string entityTypeId);
+    }
+}
